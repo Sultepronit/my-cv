75db0693449725906d172331c6c81a5136cff9b2
sultepronit <sultepronit@gmail.com>
2026-01-19 10:56:46 +0000

    initial commit!

---
+++ data.js
@@ -0,0 +1,16 @@
+const data = {
+    name: {
+        en: 'Stepan Mutsikivskyi',
+        uk: 'Степан Муціківський'
+    },
+    position: 'Software Developer (Web, JavaScript)',
+    location: 'Kyiv, Ukraine',
+    email: 'sultepronit@gmail.com',
+    phone: '+38 097 816 3650',
+    github: 'github.com/sultepronit',
+    summary: 'Self-taught developer with strong experience in JavaScript, web applications (PWA), and database-driven tools. Skilled at building practical solutions for real-world problems, from legacy systems to modern web stacks. Experienced with both frontend and backend technologies, cloud platforms, and automation of workflow tools. Fast learner with a track record of independently creating, deploying, and maintaining multiple applications.'
+};
+
+export default data;
+
+
