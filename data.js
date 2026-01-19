54c7dafb1d078d8c7552a9b541269f3ca0d60dfe
sultepronit <sultepronit@gmail.com>
2026-01-19 12:08:07 +0000

    added some raw info

--- data.js
+++ data.js
@@ -8,7 +8,30 @@ const data = {
     email: 'sultepronit@gmail.com',
     phone: '+38 097 816 3650',
     github: 'github.com/sultepronit',
-    summary: 'Self-taught developer with strong experience in JavaScript, web applications (PWA), and database-driven tools. Skilled at building practical solutions for real-world problems, from legacy systems to modern web stacks. Experienced with both frontend and backend technologies, cloud platforms, and automation of workflow tools. Fast learner with a track record of independently creating, deploying, and maintaining multiple applications.'
+    summary: 'Self-taught developer with strong experience in JavaScript, web applications (PWA), and database-driven tools. Skilled at building practical solutions for real-world problems, from legacy systems to modern web stacks. Experienced with both frontend and backend technologies, cloud platforms, and automation of workflow tools. Fast learner with a track record of independently creating, deploying, and maintaining multiple applications.',
+    techSkills: [
+        [
+            'Languages & Frameworks',
+            'JavaScript (legacy & modern ES6+), TypeScript, HTML5, CSS3, Node.js, PHP, Go, SQL'
+        ],
+        [
+            'Frontend & Web Apps',
+            'Vanilla JS, web components, Progressive Web Apps (PWA), Vite, NW.js, localStorage, IndexedDB, Firebase, Google Sheets scripting'
+        ],
+        [
+            'Backend & Databases',
+            'REST API, SQLite, Google App Engine, Google Compute Engine (VM), Docker, Nginx, HTTPS/Certbot'
+        ],
+        [
+            'Version Control & Deployment',
+            'Git, GitHub, GitHub CLI, Vercel, Render, Netlify'
+        ],
+        [
+            'Tools & Platforms',
+            // 'Linux (Lubuntu, Ubuntu Server), Windows, VS Code, Google Cloud, MS Office, LibreOffice, Google Workspace'
+            'Linux (Lubuntu, Ubuntu Server), Windows, Google Cloud, Google AI Studio, VS Code, Notepad++, Nano'
+        ]
+    ]
 };
 
 export default data;
--- main.js
+++ main.js
@@ -3,27 +3,37 @@ import data from './data.js';
 const output = document.getElementById('content');
 output.innerHTML = `<h1>Here we go!</h1>`;
 
-const parts = [
-    `<header>
-        <div>
-            <h1>${data.name.en}</h1>
-            <h2 class="aux">${data.name.uk}</h2>
-            <div class="vert-sp-1"></div>
-            <h2>${data.position}</h2>
-        </div>
-        <ul class="contacts">
-            <li>${data.location}</li>
-            <li><a href="mailto:${data.email}">${data.email}</a></li>
-            <li><a href="tel:${data.phone}">${data.phone}</a></li>
-            <li><a href="${data.github}">${data.github}</a></li>
-        </ul>
-    </header>
+const href = location.href.replace('https://', '').split('/')[0];
 
-    <main>
-        <hr>
-        <h3>Summary</h3>
-        <p>${data.summary}</p>
-    </main>`,
-];
+const parts = [`<header>
+    <div>
+        <h1>${data.name.en}</h1>
+        <h2 class="aux">${data.name.uk}</h2>
+        <div class="vert-sp-1"></div>
+        <h2>${data.position}</h2>
+    </div>
+
+    <ul class="contacts">
+        <li>${data.location}</li>
+        <li><a href="mailto:${data.email}">${data.email}</a></li>
+        <li><a href="tel:${data.phone}">${data.phone}</a></li>
+        <li><a href="${data.github}">${data.github}</a></li>
+    </ul>
+
+    <div class="abs-right-bottom print-only">Latest CV: <a href="${href}">${href}</a></div>
+</header>`];
+
+const techSkills = data.techSkills.map(
+    e => `<h4>${e[0]}:</h4>
+    <p>${e[1]}</p>`
+).join('');
+
+parts.push(`<main>
+    <hr>
+    <h3>Summary</h3>
+    <p>${data.summary}</p>
+    <h3>Technical Skills</h3>
+    ${techSkills}
+</main>`);
 
 output.innerHTML = parts.join('');
